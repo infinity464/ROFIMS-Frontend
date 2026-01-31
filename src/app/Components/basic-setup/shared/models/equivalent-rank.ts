78ab8a0f78a47afda89ca313b51e5976ea463b35
abaziz1k99 <abdulaziz1k99@outlook.com>
2026-01-31 13:28:39 +0600

    create equvalnet model

---
+++ src/app/Components/basic-setup/shared/models/equivalent-rank.ts
@@ -0,0 +1,9 @@
+export interface EquivalentRankModel {
+  equivalentNameID: number;
+  motherOrgRankId: number;
+  motherOrgId: number;
+  createdBy: string;
+  createdDate: string;
+  lastUpdatedBy: string;
+  lastUpdate: string;
+}
