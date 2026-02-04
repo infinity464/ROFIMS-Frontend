3a487cf0dc184d578d0a00739e3585f71fc09385
basir3595 <samiulbasir70122@gmail.com>
2026-02-04 08:40:17 +0600

    Add Educational Info of EMP

---
+++ src/app/services/education-info-service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '@/Core/Environments/environment';
+
+export interface EducationInfoModel {
+    employeeId: number;
+    educationId: number;
+    examName: number | null;
+    instituteType: number | null;
+    instituteName: number | null;
+    departmentName: number | null;
+    subjectName: number | null;
+    dateFrom: string | null;
+    dateTo: string | null;
+    passingYear: number | null;
+    grade: number | null;
+    remarks: string | null;
+    createdBy?: string;
+    createdDate?: string;
+    lastUpdatedBy?: string;
+    lastupdate?: string;
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class EducationInfoService {
+    private apiUrl = `${environment.apis.core}/EducationInfo`;
+
+    constructor(private http: HttpClient) {}
+
+    getByEmployeeId(employeeId: number): Observable<EducationInfoModel[]> {
+        return this.http.get<EducationInfoModel[]>(`${this.apiUrl}/GetByEmployeeId/${employeeId}`);
+    }
+
+    save(payload: Partial<EducationInfoModel>): Observable<any> {
+        return this.http.post(`${this.apiUrl}/SaveAsyn`, payload);
+    }
+
+    update(payload: Partial<EducationInfoModel>): Observable<any> {
+        return this.http.post(`${this.apiUrl}/UpdateAsyn`, payload);
+    }
+
+    delete(employeeId: number, educationId: number): Observable<any> {
+        return this.http.delete(`${this.apiUrl}/DeleteAsyn/${employeeId}/${educationId}`);
+    }
+}
